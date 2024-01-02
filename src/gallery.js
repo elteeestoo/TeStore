d5814d300e24b764d6ff096d2d222423da4a1dfa
elteeestoo <deiwid22david@gmail.com>
2024-01-02 15:43:13 +0100

    iconos

--- src/gallery.js
+++ src/gallery.js
@@ -57,7 +57,7 @@ class Gallery extends HTMLElement {
 
             .games{
                 display: grid;
-                grid-template-columns: repeat(3, 1fr);
+                grid-template-columns: repeat(4, 1fr);
                 padding: 5%;
                 gap: 4rem;
                 width: 90%;
@@ -173,13 +173,15 @@ class Gallery extends HTMLElement {
             .overlay ul {
                 width: 80%;
                 display: flex;
+                
+                justify-content: center;
                 flex-direction: column;
                 list-style: none;
-                gap: 1rem;
+                
                 font-family: "Roboto";
                 text-align: left;
                 margin: 0;
-                font-size: 18px; /* Ajusta según sea necesario */
+                font-size: 14px; /* Ajusta según sea necesario */
                 color: white;
                 -webkit-text-stroke-width: 0;
                 -webkit-text-stroke-color: transparent;
@@ -193,10 +195,8 @@ class Gallery extends HTMLElement {
                 text-decoration: underline;
                 font-family: "Roboto";
                 color: aquamarine;
-            }
-
-            .overlay h3:hover{
-                filter: brightness(0.7);
+                font-size: 14px;
+                
             }
 
             .overlay img{
@@ -205,6 +205,7 @@ class Gallery extends HTMLElement {
                 border-radius: none;
                 box-shadow: none;
                 margin-bottom: 2rem;
+                
             }
             
 
@@ -213,7 +214,20 @@ class Gallery extends HTMLElement {
             }
             .overlay .game-links{
                 display: flex;
-                gap: 1rem;
+                gap: 0.7rem;
+            }
+
+            .overlay .game-link{
+                display: flex;
+            }
+            .overlay .game-link:hover{
+                filter: brightness(0.7)
+            }
+
+            .overlay .game-link svg{
+                display: flex;
+                width: 1rem;
+                fill: aquamarine;
             }
         </style>
   
@@ -233,8 +247,14 @@ class Gallery extends HTMLElement {
                                 <li><span>Fecha de Lanzamiento:</span>22 Abril 2005</li>
                             </ul>
                             <div class="game-links">
-                                <h3 class="game-link">Saber Mas</h3>
-                                <h3 class="game-link">Añadir al carrito</h3>
+                                <div class="game-link">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>plus</title><path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" /></svg>
+                                    <h3 class="game-link">Saber Mas</h3>
+                                </div>
+                                <div class="game-link">
+                                    <h3 class="game-link">Añadir al carrito</h3>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>cart-arrow-down</title><path d="M10 0V4H8L12 8L16 4H14V0M1 2V4H3L6.6 11.6L5.2 14C5.1 14.3 5 14.6 5 15C5 16.1 5.9 17 7 17H19V15H7.4C7.3 15 7.2 14.9 7.2 14.8V14.7L8.1 13H15.5C16.2 13 16.9 12.6 17.2 12L21.1 5L19.4 4L15.5 11H8.5L4.3 2M7 18C5.9 18 5 18.9 5 20S5.9 22 7 22 9 21.1 9 20 8.1 18 7 18M17 18C15.9 18 15 18.9 15 20S15.9 22 17 22 19 21.1 19 20 18.1 18 17 18Z" /></svg>
+                                </div>
                             </div>
                             <img src="images/shrek/de-pie.png" alt="de-pie">
                         </div>
@@ -332,6 +352,44 @@ class Gallery extends HTMLElement {
                                 <h3 class="game-link">Añadir al carrito</h3>
                             </div>
                             <img src="images/shrek/diego.png" alt="diego">
+                        </div>
+                    </div>
+                </div>
+                <div class="game">
+                    <h2>Shrek 2</h2>
+                    <div class="game-image">
+                        <img src="images/shrek/shrek.jpg" alt="shrek2">
+                        <div class="overlay">
+                            <img src="images/shrek/shrek-titulo.png" alt="shrek2">
+                            <ul>
+                                <li><span>Plataforma/s:</span>PC PlayStation 2</li>
+                                <li><span>Genero:</span>Terror, Suspense</li>
+                                <li><span>Fecha de Lanzamiento:</span>22 Abril 2005</li>
+                            </ul>
+                            <div class="game-links">
+                                <h3 class="game-link">Saber Mas</h3>
+                                <h3 class="game-link">Añadir al carrito</h3>
+                            </div>
+                            <img src="images/shrek/boca.png" alt="boca">
+                        </div>
+                    </div>
+                </div>
+                <div class="game">
+                    <h2>Shrek 2</h2>
+                    <div class="game-image">
+                        <img src="images/shrek/shrek.jpg" alt="shrek2">
+                        <div class="overlay">
+                            <img src="images/shrek/shrek-titulo.png" alt="shrek2">
+                            <ul>
+                                <li><span>Plataforma/s:</span>PC PlayStation 2</li>
+                                <li><span>Genero:</span>Terror, Suspense</li>
+                                <li><span>Fecha de Lanzamiento:</span>22 Abril 2005</li>
+                            </ul>
+                            <div class="game-links">
+                                <h3 class="game-link">Saber Mas</h3>
+                                <h3 class="game-link">Añadir al carrito</h3>
+                            </div>
+                            <img src="images/shrek/boca.png" alt="boca">
                         </div>
                     </div>
                 </div>
